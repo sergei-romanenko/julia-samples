7ed561800722b42c0c5c7418eabb92ddabdf952e
Sergei Romanenko <sergei.romanenko@supercompilers.ru>
2024-01-18 22:53:00 +0300

    CUDA_Plaground -> CUDA_Playground

--- CUDA_Plaground/src/CUDA_Plaground.jl
+++
@@ -1,5 +0,0 @@
-module CUDA_Plaground
-
-greet() = print("Hello World!")
-
-end # module CUDA_Plaground
---
+++ CUDA_Playground/src/CUDA_Playground.jl
@@ -0,0 +1,5 @@
+module CUDA_Playground
+
+greet() = print("Hello World!")
+
+end # module CUDA_Playground
